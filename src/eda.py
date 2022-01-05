4a191be63832f86d9338e173e4c00c3ad2c862a8
jjz17 <jasonjzhang17@gmail.com>
2022-01-05 10:40:42 -0500

    Changed figure sizes

--- src/eda.py
+++ src/eda.py
@@ -1,13 +1,16 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
 import os
+
+import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
+
 
 # Saves images as png files
 def save_figure(fig_name: str):
     plt.savefig(f'..{os.path.sep}visualizations{os.path.sep}{fig_name}.png')
 
-#%%
+
+# %%
 
 data = pd.read_csv(f'..{os.path.sep}data{os.path.sep}wrangled_data.csv')
 # url = 'https://raw.githubusercontent.com/jjz17/Tennis-Match-Outcome-Prediction/main/data/wrangled_data.csv'
@@ -55,11 +58,15 @@ less_data = data[most_important].reset_index()
 sns.pairplot(less_data)
 plt.show()
 
+# %%
 plt.figure(figsize=(3, 3))
-sns.relplot(x='s1 fs points', y='s2 fs points', hue='s1 win', data=less_data)
+sns.relplot(x='s1 fs points', y='s2 fs points', hue='s1 win', data=data)
 plt.title('S1 and S2 First Set Points with Match Outcome')
+fig = plt.gcf()
+fig.set_size_inches(3, 3)
 save_figure('s1_s2_points_win_relplot')
 plt.show()
+# %%
 
 sns.relplot(x='s1 fs momentum', y='s2 fs momentum', hue='s1 win', data=less_data)
 plt.show()
@@ -78,11 +85,11 @@ plt.show()
 sns.relplot(x='s1 fs momentum', y='s2 fs momentum', hue='comeback', data=data)
 plt.show()
 
-#%%
+# %%
 sns.countplot(x='comeback', data=data)
 plt.title('Comeback Frequency')
 plt.show()
-#%%
+# %%
 # data['s1 fs points'].hist()
 # plt.show()
 # data['s2 fs points'].hist()
