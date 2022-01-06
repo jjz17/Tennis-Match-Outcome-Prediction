137bfd36a573fe6952812454b70c00618affc738
jjz17 <jasonjzhang17@gmail.com>
2022-01-05 20:13:11 -0500

    Removed duplicate code from app file

--- src/tennis-prediction-app.py
+++ src/tennis-prediction-app.py
@@ -8,163 +8,13 @@ import streamlit as st
 import pickle
 import joblib
 from PIL import Image
+import functions as f
+import os
 
 ######################
 # Custom function
 ######################
 ## Calculate set stats
-
-def momentum_check(pbp, tiebreak=False):
-    momentum_threshold = 3
-    if tiebreak:
-        momentum_threshold = 2
-
-    s_momentum = True
-    momentum = 0
-    if len(pbp) >= momentum_threshold:
-        if set(pbp[:momentum_threshold]).issubset({'A', 'S'}):
-            momentum += 1
-        if set(pbp[:momentum_threshold]).issubset({'R', 'D'}):
-            momentum += 1
-            s_momentum = False
-    return s_momentum, momentum
-
-# Function to extract relevant data from a game pbp string
-def extract_game_data(game_pbp):
-    s_points = 0
-    r_points = 0
-    s_momentum = 0
-    r_momentum = 0
-    breaks = 0
-    aces = 0
-
-    # iterate through each point
-    for i in range(len(game_pbp)):
-        point = game_pbp[i]
-
-        # Handle momentum quantification
-        serve_momentum, m_count = momentum_check(game_pbp[i:])
-        if m_count > 0:
-            if serve_momentum:
-                s_momentum += 1
-            else:
-                r_momentum += 1
-
-        if point == 'S':
-            s_points += 1
-        elif point == 'A':
-            aces += 1
-            s_points += 1
-        elif point == 'R':
-            r_points += 1
-        elif point == 'D':
-            r_points += 1
-        else:
-            s_points += 0
-
-    if r_points > s_points:
-        breaks = 1
-
-    return [s_points, r_points, s_momentum, r_momentum, breaks, aces]
-
-
-# Function to extract relevant data from a tiebreak
-def extract_tiebreak_data(tiebreak_pbp):
-    p1_points = 0
-    p2_points = 0
-    p1_momentum = 0
-    p2_momentum = 0
-    p1_aces = 0
-    p2_aces = 0
-
-    p1_serving = False
-
-    splits = str(tiebreak_pbp).split('/')
-
-    # iterate through each point
-    for points in splits:
-        # toggle boolean flag
-        p1_serving = not p1_serving
-
-        s_points = 0
-        r_points = 0
-        s_momentum = 0
-        r_momentum = 0
-        aces = 0
-
-        # Handle momentum quantification
-        serve_momentum, m_count = momentum_check(points, tiebreak=True)
-        if m_count > 0:
-            if serve_momentum:
-                s_momentum += 1
-            else:
-                r_momentum += 1
-
-        for point in points:
-
-            if point == 'S':
-                s_points += 1
-            elif point == 'A':
-                aces += 1
-                s_points += 1
-            elif point == 'R':
-                r_points += 1
-            elif point == 'D':
-                r_points += 1
-            else:
-                s_points += 0
-
-        if p1_serving:
-            p1_points += s_points
-            p2_points += r_points
-            p1_momentum += s_momentum
-            p2_momentum += r_momentum
-            p1_aces += aces
-        else:
-            p2_points += s_points
-            p1_points += r_points
-            p2_momentum += s_momentum
-            p1_momentum += r_momentum
-            p2_aces += aces
-
-    return [p1_points, p2_points, p1_momentum, p2_momentum, p1_aces, p2_aces]
-
-def is_p1_set_winner(last_game_pbp, num_games):
-
-    # If tiebreak decider
-    if num_games > 12:
-        p1_points = 0
-        p2_points = 0
-
-        switches = last_game_pbp.split('/')
-        p1_serves = switches[::2]
-        p2_serves = switches[1::2]
-        for switch in p1_serves:
-            p1_points += switch.count('A')
-            p1_points += switch.count('S')
-            p2_points += switch.count('R')
-            p2_points += switch.count('D')
-        for switch in p2_serves:
-            p2_points += switch.count('A')
-            p2_points += switch.count('S')
-            p1_points += switch.count('R')
-            p1_points += switch.count('D')
-        return p1_points > p2_points
-    else:
-        s_points = 0
-        r_points = 0
-
-        s_points += last_game_pbp.count('A')
-        s_points += last_game_pbp.count('S')
-        r_points += last_game_pbp.count('R')
-        r_points += last_game_pbp.count('D')
-
-        # If p2 serves
-        if num_games % 2 == 0:
-            return r_points > s_points
-        # If p1 serves
-        else:
-            return s_points > r_points
 
 def compute_set_stats(set_pbp):
     # create list of games in the match
@@ -190,7 +40,7 @@ def compute_set_stats(set_pbp):
 
     # Aggregate data from p1 serving games
     for game in p1_s_games:
-        game_data = extract_game_data(game)
+        game_data = f.extract_game_data(game)
         fs_s1_points_count += game_data[0]
         fs_s2_points_count += game_data[1]
         fs_s1_momentum_count += game_data[2]
@@ -200,7 +50,7 @@ def compute_set_stats(set_pbp):
 
     # Aggregate data from p2 serving games
     for game in p2_s_games:
-        game_data = extract_game_data(game)
+        game_data = f.extract_game_data(game)
         fs_s2_points_count += game_data[0]
         fs_s1_points_count += game_data[1]
         fs_s2_momentum_count += game_data[2]
@@ -209,7 +59,7 @@ def compute_set_stats(set_pbp):
         fs_s2_aces_count += game_data[5]
 
     # Aggregate data from tiebreak
-    tb_data = extract_tiebreak_data(tiebreak)
+    tb_data = f.extract_tiebreak_data(tiebreak)
     fs_s1_points_count += tb_data[0]
     fs_s2_points_count += tb_data[1]
     fs_s1_momentum_count += tb_data[2]
@@ -217,7 +67,7 @@ def compute_set_stats(set_pbp):
     fs_s1_aces_count += tb_data[4]
     fs_s2_aces_count += tb_data[5]
 
-    p1_fs_win = is_p1_set_winner(games[-1], len(games))
+    p1_fs_win = f.is_s1_set_winner(games[-1], len(games))
 
     if p1_fs_win:
         p1_fs_win = 1
@@ -307,7 +157,7 @@ st.table(info)
 
 # scaled_stats2 = scale_features(stats)
 st.header('Scaled set stats')
-scaler = joblib.load('../models/tennis_minmax_scaler')
+scaler = joblib.load(f'..{os.path.sep}models{os.path.sep}tennis_minmax_scaler')
 scaled_stats = scaler.transform(np.array(stats).reshape(1,-1))
 scaled_stats
 # scaled_stats2
@@ -317,7 +167,7 @@ scaled_stats
 ######################
 
 # Reads in saved model
-load_model = pickle.load(open('../models/tennis_prediction_model.pk1', 'rb'))
+load_model = pickle.load(open(f'..{os.path.sep}models{os.path.sep}logreg_model.pickle', 'rb'))
 
 
 
